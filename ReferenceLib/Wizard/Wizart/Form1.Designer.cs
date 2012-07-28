7bfdd081f949240e24a74c2987e5acabcaafccd2
sunpander <sunpander@163.com>
2012-07-28 14:17:23 +0800

    wizard

---
+++ ReferenceLib/Wizard/Wizart/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Wizart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wizard1 = new Orasoft.Windows.Forms.Wizard();
+            this.wizardPage1 = new Orasoft.Windows.Forms.WizardPage();
+            this.wizardPage2 = new Orasoft.Windows.Forms.WizardPage();
+            this.wizardPage3 = new Orasoft.Windows.Forms.WizardPage();
+            this.wizardPage4 = new Orasoft.Windows.Forms.WizardPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wizard1.SuspendLayout();
+            this.wizardPage1.SuspendLayout();
+            this.wizardPage2.SuspendLayout();
+            this.wizardPage3.SuspendLayout();
+            this.wizardPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wizard1
+            // 
+            this.wizard1.Controls.Add(this.wizardPage4);
+            this.wizard1.Controls.Add(this.wizardPage3);
+            this.wizard1.Controls.Add(this.wizardPage2);
+            this.wizard1.Controls.Add(this.wizardPage1);
+            this.wizard1.Location = new System.Drawing.Point(0, 0);
+            this.wizard1.Name = "wizard1";
+            this.wizard1.Pages.AddRange(new Orasoft.Windows.Forms.WizardPage[] {
+            this.wizardPage1,
+            this.wizardPage2,
+            this.wizardPage3,
+            this.wizardPage4});
+            this.wizard1.Size = new System.Drawing.Size(284, 262);
+            this.wizard1.TabIndex = 0;
+            // 
+            // wizardPage1
+            // 
+            this.wizardPage1.Controls.Add(this.label1);
+            this.wizardPage1.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage1.Name = "wizardPage1";
+            this.wizardPage1.Size = new System.Drawing.Size(284, 214);
+            this.wizardPage1.TabIndex = 10;
+            // 
+            // wizardPage2
+            // 
+            this.wizardPage2.Controls.Add(this.comboBox1);
+            this.wizardPage2.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage2.Name = "wizardPage2";
+            this.wizardPage2.Size = new System.Drawing.Size(284, 214);
+            this.wizardPage2.TabIndex = 11;
+            // 
+            // wizardPage3
+            // 
+            this.wizardPage3.Controls.Add(this.button1);
+            this.wizardPage3.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage3.Name = "wizardPage3";
+            this.wizardPage3.Size = new System.Drawing.Size(284, 214);
+            this.wizardPage3.TabIndex = 12;
+            // 
+            // wizardPage4
+            // 
+            this.wizardPage4.Controls.Add(this.checkBox1);
+            this.wizardPage4.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage4.Name = "wizardPage4";
+            this.wizardPage4.Size = new System.Drawing.Size(284, 214);
+            this.wizardPage4.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(90, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(109, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.wizard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.wizard1.ResumeLayout(false);
+            this.wizardPage1.ResumeLayout(false);
+            this.wizardPage1.PerformLayout();
+            this.wizardPage2.ResumeLayout(false);
+            this.wizardPage3.ResumeLayout(false);
+            this.wizardPage4.ResumeLayout(false);
+            this.wizardPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Orasoft.Windows.Forms.Wizard wizard1;
+        private Orasoft.Windows.Forms.WizardPage wizardPage4;
+        private Orasoft.Windows.Forms.WizardPage wizardPage3;
+        private Orasoft.Windows.Forms.WizardPage wizardPage2;
+        private Orasoft.Windows.Forms.WizardPage wizardPage1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
